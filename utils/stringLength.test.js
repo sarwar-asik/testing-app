522408a9833a82763e3c01822d2cbf521837d64d
sarwar-asik <sarwarasik@gmail.com>
2023-11-10 02:28:34 +0600

    to error string lenth

--- utils/stringLength.test.js
+++ utils/stringLength.test.js
@@ -1,11 +1,16 @@
 import { expect, it } from "vitest";
 import { stringLength } from "./stringLength";
 
-it("return length for non empty",()=>{
-    expect(stringLength("hello")).toBe(5)
+it("return length for non empty", () => {
+  expect(stringLength("hello")).toBe(5);
+});
 
-})
+it("return 0 for empty string", () => {
+  expect(stringLength("")).toBe(0);
+});
 
-it('return 0 for empty string',()=>{
-    expect(stringLength("")).toBe(0)
-})
+it("return error for not string input", () => {
+  expect(() => stringLength(null)).toThrow();
+  expect(() => stringLength(undefined)).toThrow();
+  expect(stringLength(123)).toBeUndefined()
+});

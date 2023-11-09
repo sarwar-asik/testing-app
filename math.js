b1ac18d3be195e66914c58f93a6f54eec25b2223
sarwar-asik <sarwarasik@gmail.com>
2023-11-10 01:41:51 +0600

    math nan run

--- math.js
+++ math.js
@@ -2,7 +2,7 @@ export function add(numbers) {
   let sum = 0;
   for (const num of numbers) {
     // console.log(num);
-    sum += num;
+    sum +=  + num;
   }
   return sum;
 }
--- math.test.js
+++ math.test.js
@@ -2,18 +2,43 @@ import { expect, it } from "vitest";
 import { add } from "./math";
 
 it("sum of the numbers", () => {
+  ///! A A A
 
-    ///! A A A
+  //Arranger
+  const numbers = [1, 2, 3, 4, 5];
+  const expectedResult = numbers.reduce((acc, current) => acc + current);
 
-//Arranger
-
-    const numbers =[1,2,3,4,5]
-    const expectedResult =numbers.reduce((acc,current)=>acc+current)
-
-
-    /// Action
+  /// Action
   const result = add(numbers);
 
-  /// Assertion 
+  /// Assertion
+  expect(result).toBe(expectedResult);
+});
+
+it("sum of the name at least 1 invalid number", () => {
+  ///! A A A
+
+  //Arranger
+  const numbers = [1, 2, 3, "invalid", 5];
+  const expectedResult = numbers.reduce((acc, current) => acc + current);
+
+  /// Action
+  const result = add(numbers);
+
+  /// Assertion
+  expect(result).toBeNaN();
+});
+
+it("sum of the name at array of numeric string", () => {
+  ///! A A A
+
+  //Arranger
+  const numbers = [1, 2, 3, "4", 5];
+  const expectedResult = numbers.reduce((acc, current) => +acc + +current);
+
+  /// Action
+  const result = add(numbers);
+
+  /// Assertion
   expect(result).toBe(expectedResult);
 });

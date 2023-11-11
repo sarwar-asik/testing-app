8aa1962fb67f92bac136d1a685d6f8a047ebaf2d
sarwar-asik <sarwarasik@gmail.com>
2023-11-11 09:21:45 +0600

    fs mock error

--- utils/io.test.js
+++ utils/io.test.js
@@ -1,12 +1,21 @@
 import { expect, it, vi } from "vitest";
 import { saveTokenToFiles } from "./io";
-import {promises as fs} from "fs"
+import { promises as fs } from "fs";
 
-vi.mock("fs")
+vi.mock("fs");
 
-it("store token to the file",()=>{
-    const data ="my token";
-    const fileName ="token.txt";
-    saveTokenToFiles(data,fileName);
-    expect(fs.writeFile).toHaveBeenCalled()
-})
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => args(args.length - 1),
+    },
+  };
+});
+
+it("store token to the file", () => {
+  const data = "my token";
+  const fileName = "token.txt";
+  saveTokenToFiles(data, fileName);
+  //   expect(fs.writeFile).toHaveBeenCalled();
+  expect(fs.writeFile).toBeCalledWith(fileName, data);
+});

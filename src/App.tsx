429bc9219f96b937b0ed4116fedfbbc7444fb0cc
sarwar-asik <sarwarasik@gmail.com>
2023-11-14 03:20:11 +0600

    app test error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-
-
-  return (
-  
-      <div className="">Hello World</div>
-
-  );
+  return <div className="">Hello World</div>;
 }
 
 export default App;
--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
-import { expect, test } from "vitest";
+import App from "./App"
 
 test("should run hello world", () => {
   render(<App />);

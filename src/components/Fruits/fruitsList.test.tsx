f77b3c0a98354d1d4b26db4481c5bb21fd7d2a56
sarwar-asik <sarwarasik@gmail.com>
2023-11-16 01:30:12 +0600

    installion jest

--- src/components/Fruits/fruitsList.test.tsx
+++ src/components/Fruits/fruitsList.test.tsx
@@ -1,14 +1,15 @@
-import { render, screen } from "@testing-library/react"
-import FruitsList from "./FruitsList"
-import { fruits } from "../../const/fruits"
+import { render, screen } from "@testing-library/react";
+import FruitsList from "./FruitsList";
+import { fruits } from "../../const/fruits";
+describe("FruitsList()", () => {
+  it("should be list of fruits", () => {
+    render(<FruitsList fruits={fruits} />);
+    const list = screen.getByRole("list");
+    const listItem = screen.getAllByRole("listitem");
 
-describe("FruitsList()",()=>{
-    it("should be list of fruits",()=>{
-        render(<FruitsList fruits={fruits}/>)
-        const list = screen.getByRole("list")
-        const listItem = screen.getAllByRole("listitem")
-        //@ts-ignore
-        expect(list).toBeInTheDocument()
-    })
-})
-
+    //@ts-ignore
+    expect(list).toBeInTheDocument();
+    //@ts-ignore
+    expect(listItem).toHaveLength(fruits.length);
+  });
+});
